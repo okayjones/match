d1ff6556c93f0990fdab2770b2685191ba561b07
Logan Jones <loganj@zillowgroup.com>
2020-09-02 09:13:51 -0700

    fix id declarations

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let card0 = document.getElementById('card0');
 let card1 = document.getElementById('card1');
 let card2 = document.getElementById('card2');
 let card3 = document.getElementById('card3');
-let card4 = document.getElementById('card0');
-let card5 = document.getElementById('card1');
-let card6 = document.getElementById('card2');
-let card7 = document.getElementById('card3');
+let card4 = document.getElementById('card4');
+let card5 = document.getElementById('card5');
+let card6 = document.getElementById('card6');
+let card7 = document.getElementById('card7');
 let score = document.getElementById('score');
 let reset = document.getElementById('reset');
 
